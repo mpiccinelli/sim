9cf3fd19837b69fef372e5bfd5328e89257098cb
Marlos Piccinelli <92126412+mpiccinelli@users.noreply.github.com>
2021-11-19 12:02:32 -0300

    Edit Map

--- Office.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Office" tilewidth="32" tileheight="32" tilecount="1424" columns="16">
- <image source="src/assets/tileset-office.png" width="512" height="2848"/>
-</tileset>
