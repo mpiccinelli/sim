41719a1e720454ffdf84778f0f92bd89b110deee
Marlos Piccinelli <92126412+mpiccinelli@users.noreply.github.com>
2022-01-24 22:07:39 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,3 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
-
-let helloWorldPopup;
-
-// Open the popup when we enter a given zone
-helloWorldPopup = WA.room.onEnterLayer("myZone").subscribe(() => {
-    WA.ui.openPopup("popupRectangle", 'Hello world!', [{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    }]);
-});
-
-// Close the popup when we leave the zone.
-WA.room.onLeaveLayer("myZone").subscribe(() => {
-    helloWorldPopup.close();
-})
