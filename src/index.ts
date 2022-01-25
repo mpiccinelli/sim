02b56e7f4383c94742d6ae1b472d7ed41decf22b
Marlos Piccinelli <92126412+mpiccinelli@users.noreply.github.com>
2022-01-24 22:39:59 -0300

    add img

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,3 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
-
-WA.room.onEnterZone('clockZone').subscribe(() => {
-    WA.chat.sendChatMessage("Hello!", 'Mr Robot');
-});
-
-WA.room.onLeaveLayer('clockZone').subscribe(() => {
-    WA.chat.sendChatMessage("Goodbye!", 'Mr Robot');
-});
