24554f32a4428eb3447f1b0e8c6125ded44acc87
Marlos Piccinelli <92126412+mpiccinelli@users.noreply.github.com>
2022-01-24 22:14:12 -0300

    teste deploy

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,21 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
+
+let currentPopup: any = undefined;
+const today = new Date();
+const time = today.getHours() + ":" + today.getMinutes();
+
+WA.room.onEnterLayer('clockZone').subscribe(() => {
+    console.log('toto')
+    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+})
+
+WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
