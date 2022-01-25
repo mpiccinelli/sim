755fc3e8af8688841aa01cea9d2543e0993f5da5
Marlos Piccinelli <92126412+mpiccinelli@users.noreply.github.com>
2022-01-24 21:10:23 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,4 +22,21 @@ function closePopUp(){
     }
 }
 
-WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.room.onEnterLayer("myZone").subscribe(() => {
+    WA.ui.openPopup("popupRectangle", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("myZone").subscribe(() => {
+    helloWorldPopup.close();
+});
